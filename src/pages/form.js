df119ee10e71563f96ac7b1aca1c464c09423469
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-01-11 01:20:21 -0500

    email JS works

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,27 +1,33 @@
-import React from "react"
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const handleLoginSubmit = (e) => {
+export default function FormPage () {
 
-  let templateParams = {
-    user_name: name,
-    user_email: email,
-    message_html: "Hi!",
-   }
-   
-   emailjs.send(
-   "hawkaton.us@gmail.com",
-   'template_OztCtOOX',
-    templateParams,
-   'user_qkmMCvLoKG06KjZD3u8VY'
-  )
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
 
-}
+  const handleLoginSubmit = (e) => {
+  e.preventDefault();
 
-const FormPage = () => (
+    let templateParams = {
+      user_name: name,
+      user_email: email,
+      message_html: "Hi!",
+     }
+
+     emailjs.send(
+     "hawkaton.us@gmail.com",
+     'template_OztCtOOX',
+      templateParams,
+     'user_qkmMCvLoKG06KjZD3u8VY'
+    )
+
+  }
+
+  return (
 
     <Layout>
         <SEO title="FormPage"/>
@@ -41,11 +47,11 @@ const FormPage = () => (
                          <form onSubmit={handleLoginSubmit}>
                            <label>
                              Name:
-                             <input type="text" name="name" />
+                             <input type="text" name="name" onChange={e => setName(e.target.value)} value={name}/>
                            </label>
                            <label>
                              E-mail:
-                             <input type="text" name="email" />
+                             <input type="text" name="email" onChange={e => setEmail(e.target.value)} value={email}/>
                            </label>
                           <input type="submit" value="Submit" />
                          </form>
@@ -55,6 +61,44 @@ const FormPage = () => (
             </div>
         </div>
     </Layout>
-)
 
-export default FormPage
+  );
+}
+
+// const FormPage = () => (
+//
+//     <Layout>
+//         <SEO title="FormPage"/>
+//         <div className={"container"}>
+//             <div className={"content"}>
+//                 <div className={"title"}>
+//                     <h1>Form</h1>
+//                 </div>
+//
+//                 <div className={"row"}>
+//                     <div className={"col-7"}>
+//                         <p>!!!!!</p>
+//                     </div>
+//
+//                     <div className={"col-5"}>
+//                         <div className={"contact-items"}>
+//                          <form onSubmit={handleLoginSubmit}>
+//                            <label>
+//                              Name:
+//                              <input type="text" name="name" />
+//                            </label>
+//                            <label>
+//                              E-mail:
+//                              <input type="text" name="email" />
+//                            </label>
+//                           <input type="submit" value="Submit" />
+//                          </form>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </Layout>
+// )
+//
+// export default FormPage
