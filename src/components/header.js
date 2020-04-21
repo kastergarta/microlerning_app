aca26a355b1cbce6955643a64f825d4ece928022
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-04-21 14:53:05 -0400

    AuthO implementation 1.1

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,12 @@ import {Link} from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import logo from "../../static/images/GURU_logo_2.jpg"
+import { useAuth0 } from "/Users/denisbasyrov/Desktop/App/microlearning_app/src/react-auth0-spa.js";
+
+
 
 const Header = ({siteTitle}) => (
+    
     <header>
         <div className={"container"}>
             <div className={"top-menu"}>
