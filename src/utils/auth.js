de1e3421fabd0695335405601e6aa55e5c35442d
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-04-21 22:31:16 -0400

    AuthO implementation 1.4 fixed account+login+logout

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,57 +13,67 @@ const auth = isBrowser
     })
   : {}
 
-  const tokens = {
-    accessToken: false,
-    idToken: false,
-    expiresAt: false,
+const tokens = {
+  accessToken: false,
+  idToken: false,
+  expiresAt: false,
+}
+
+let user = {}
+
+export const isAuthenticated = () => {
+  if (!isBrowser) {
+    return;
   }
-  
-  let user = {}
-  
-  export const isAuthenticated = () => {
-    if (!isBrowser) {
-      return;
-    }
-  
-    return localStorage.getItem("isLoggedIn") === "true"
+
+  return localStorage.getItem("isLoggedIn") === "true"
+}
+
+export const login = () => {
+  if (!isBrowser) {
+    return
   }
-  
-  export const login = () => {
-    if (!isBrowser) {
-      return
-    }
-  
-    auth.authorize()
+
+  auth.authorize()
+}
+
+const setSession = (cb = () => {}) => (err, authResult) => {
+  if (err) {
+    navigate("/")
+    cb()
+    return
   }
-  
-  const setSession = (cb = () => {}) => (err, authResult) => {
-    if (err) {
-      navigate("/")
-      cb()
-      return
-    }
-  
-    if (authResult && authResult.accessToken && authResult.idToken) {
-      let expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
-      tokens.accessToken = authResult.accessToken
-      tokens.idToken = authResult.idToken
-      tokens.expiresAt = expiresAt
-      user = authResult.idTokenPayload
-      localStorage.setItem("isLoggedIn", true)
-      navigate("/account")
-      cb()
-    }
+
+  if (authResult && authResult.accessToken && authResult.idToken) {
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
+    tokens.accessToken = authResult.accessToken
+    tokens.idToken = authResult.idToken
+    tokens.expiresAt = expiresAt
+    user = authResult.idTokenPayload
+    localStorage.setItem("isLoggedIn", true)
+    navigate("/account")
+    cb()
   }
-  
-  export const handleAuthentication = () => {
-    if (!isBrowser) {
-      return;
-    }
-  
-    auth.parseHash(setSession())
+}
+
+export const silentAuth = callback => {
+  if (!isAuthenticated()) return callback()
+  auth.checkSession({}, setSession(callback))
+}
+
+export const handleAuthentication = () => {
+  if (!isBrowser) {
+    return;
   }
-  
-  export const getProfile = () => {
-    return user
-  }
+
+  auth.parseHash(setSession())
+}
+
+export const getProfile = () => {
+  return user
+}
+
+export const logout = () => {
+  localStorage.setItem("isLoggedIn", false)
+  auth.logout()
+}
